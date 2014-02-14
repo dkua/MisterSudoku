396e5030400020d4a682c239857693a88a460dea
David Kua <david@kua.io>
2014-02-14 09:34:21 -0500

    Added 10 minute delay to each tweet.

--- mister_sudoku.go
+++ mister_sudoku.go
@@ -4,14 +4,18 @@ import (
 	"bot"
 	"fmt"
 	"net/url"
+	"time"
 )
 
 func main() {
 	api := bot.GetTwitterApi()
-	puzzle := bot.CreateTweet()
-	tweet, err := api.PostTweet(puzzle, url.Values{})
-	if err != nil {
-		fmt.Printf("%v\n%v", "Something went wrong", err)
+	for {
+		puzzle := bot.CreateTweet()
+		tweet, err := api.PostTweet(puzzle, url.Values{})
+		if err != nil {
+			fmt.Printf("%v\n%v", "Something went wrong", err)
+		}
+		fmt.Println(tweet)
+		time.Sleep(600 * time.Second)
 	}
-	fmt.Println(tweet)
 }
