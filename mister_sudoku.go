bd8237cdbb1e0e5a7af23df85557dfad549f2d85
David Kua <david@kua.io>
2014-02-14 09:06:30 -0500

    Added tweeting functionality to the bot.

--- mister_sudoku.go
+++ mister_sudoku.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"bot"
+	"fmt"
+	"net/url"
 )
 
 func main() {
+	api := bot.GetTwitterApi()
+	puzzle := bot.CreateTweet()
+	tweet, err := api.PostTweet(puzzle, url.Values{})
+	if err != nil {
+		fmt.Printf("%v\n%v", "Something went wrong", err)
+	}
+	fmt.Println(tweet)
 }
--- src/bot/bot.go
+++ src/bot/bot.go
@@ -1,7 +1,43 @@
 package bot
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/dkua/go-sudoku"
-	"net/url"
+	"os"
 )
+
+type Configuration struct {
+	ConsumerKey    string
+	ConsumerSecret string
+	AccessToken    string
+	AccessSecret   string
+}
+
+func CreateTweet() string {
+	puzzle := sudoku.CreatePuzzle(17) // 17 is the min givens for a unique solution
+	var buffer bytes.Buffer
+	for i := 0; i < len(puzzle); i += 9 {
+		row := fmt.Sprintf("%v\n", string(puzzle[i:i+9]))
+		buffer.WriteString(row)
+	}
+	buffer.WriteString("@MissSudoku")
+	return buffer.String()
+}
+
+func GetTwitterApi() *anaconda.TwitterApi {
+	file, _ := os.Open("settings.json")
+	decoder := json.NewDecoder(file)
+	settings := &Configuration{}
+	decoder.Decode(&settings)
+	fmt.Println(settings.AccessToken)
+	fmt.Println(settings.AccessSecret)
+
+	anaconda.SetConsumerKey(settings.ConsumerKey)
+	anaconda.SetConsumerSecret(settings.ConsumerSecret)
+	api := anaconda.NewTwitterApi(settings.AccessToken, settings.AccessSecret)
+
+	return api
+}
